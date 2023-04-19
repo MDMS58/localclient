35815aeedd4a130bbfc85c49763910d0aaa0f15a
Isaac <isaac@isaac-Inspiron-3576>
2023-04-18 21:49:13 -0600

    arduino

--- widget.cpp
+++ widget.cpp
@@ -5,7 +5,7 @@
 #include <string>
 #include <iostream>
 #include <stdlib.h>
-#include <Qtimer>
+#include <QTimer>
 
 QString message="";
 bool flag=false;
@@ -78,7 +78,19 @@ void widget::serialCom() {
             charArray[in.size()] = '\n';
 
             controller.writeSerialPort(charArray, MAX_DATA_LENGTH);
-            qDebug()<<controller.readSerialPort(out, MAX_DATA_LENGTH);
+            QVariant data=controller.readSerialPort(out, MAX_DATA_LENGTH);
+            if(data=="W" ){
+                message="W";
+
+                ui->label->setText(message);
+                socket->write(message.toUtf8());
+            }
+            if(data=="S" ){
+                message="W";
+
+                ui->label->setText(message);
+                socket->write(message.toUtf8());
+            }
 
         } else {
             std::cout<<"no conncection ";
